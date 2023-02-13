6a6db3ee60c08ceb0bc0e7703f18351296f2f09f
gilles <gsouton@gmail.com>
2023-02-13 14:47:31 +0100

    feat(CSharp): implement task on C#

---
+++ cSharp/console-app/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace console_app;
+class Program {
+
+    static string ParseArguments(string[] args) {
+        if (args.Length == 0) {
+            Console.Error.WriteLine("Wrong use: word_count <file1> <file2> ... <file n>");
+            throw new ApplicationException();
+        }
+        return args[0];
+    }
+
+    static void Main(string[] args) {
+
+
+        // TODO: refactor to the following
+        //
+        // string[] paths = ParseArguments(args);
+        // WordCounter wc = new WordCounter(paths);
+        // wc.Count();
+        // wc.LogCount();
+        //
+        string path = ParseArguments(args);
+        string[] fileContent = File.ReadAllLines(path);
+        Dictionary<string, uint> wordCount = new Dictionary<string, uint>();
+        foreach (string line in fileContent) {
+            string[] words = line.Split(" ");
+            foreach (string word in words) {
+                uint count;
+                if (wordCount.TryGetValue(word, out count)) {
+                    wordCount[word] = ++count;
+                }
+                else {
+                    wordCount.Add(word, 1);
+                }
+            }
+        }
+        foreach (var entry in wordCount) {
+            Console.WriteLine($"{entry.Key}: {entry.Value}");
+        }
+    }
+}
---
+++ cSharp/test-library/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace test_library;
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.NotEqual(42, 24);
+
+    }
+}
